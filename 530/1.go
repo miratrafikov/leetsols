a30bdacb93289b27d7dd585d75514f4c756fb341
Mirat Rafikov <miratworking@gmail.com>
2023-02-17 21:48:59 +0300

    783 530 go

---
+++ 530/1.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getMinimumDifference(root *TreeNode) int {
+    values := []int{}
+    collectValues(root, &values)
+
+    minDiff := values[1] - values[0]
+    for i := 2; i < len(values); i++ {
+        currDiff := values[i] - values[i - 1]
+        if currDiff < minDiff {
+            minDiff = currDiff
+        }
+    }
+
+    return minDiff
+}
+
+func collectValues(root *TreeNode, values *[]int) {
+    if root == nil {
+        return
+    }
+
+    collectValues(root.Left, values)
+    *values = append(*values, root.Val)
+    collectValues(root.Right, values)
+}
