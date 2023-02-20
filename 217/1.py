9bb843b11a6ee74d1fcb559def814d3e7263e4c6
Mirat Rafikov <miratworking@gmail.com>
2023-02-21 02:05:16 +0300

    217 py

---
+++ 217/1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = {}
+        for num in nums:
+            if num in seen:
+                return True
+            
+            seen[num] = True
+        
+        return False
