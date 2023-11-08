bf7b357e50992939e5594acc7db5a8a940b47219
Mirat Rafikov <miratworking@gmail.com>
2023-11-09 01:02:26 +0300

    2923 go

--- 2923/1.go
+++ 2923/1.go
@@ -1,16 +1,16 @@
 func findChampion(grid [][]int) int {
     n := len(grid)
     var teamThatNeverLost int
-    for teamToTestIfItEverLost := 0; teamToTestIfItEverLost < n; teamToTestIfItEverLost++ {
+    for teamToTestIfEverLost := 0; teamToTestIfEverLost < n; teamToTestIfEverLost++ {
         didLose := false
         for rivalTeam := 0; rivalTeam < n; rivalTeam++ {
-            if grid [rivalTeam][teamToTestIfItEverLost] == 1 {
+            if teamToTestIfEverLost != rivalTeam && grid [rivalTeam][teamToTestIfEverLost] == 1 {
                 didLose = true
                 break
             }
         }
         if !didLose {
-            teamThatNeverLost = teamToTestIfItEverLost
+            teamThatNeverLost = teamToTestIfEverLost
             break
         }
     }
