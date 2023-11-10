be98d0de9506aa64fabf8704a322b66810a9ddd6
Mirat Rafikov <miratworking@gmail.com>
2023-11-10 14:07:41 +0300

    49 go

---
+++ 49/1.go
@@ -0,0 +1,25 @@
+func groupAnagrams(strs []string) [][]string {
+    descriptorLength := int('z') - int('a') + 1
+    descriptorToAnagramGroup := make(map[string][]string)
+    for _, s := range strs {
+        lettersCountInS := make([]int, descriptorLength)
+        for _, letter := range s {
+            lettersCountInS[int(letter) - int('a')] += 1
+        }
+        fmt.Println(lettersCountInS)
+        descriptor := ""
+        for i, count := range lettersCountInS {
+            for j := 0; j < count; j++ {
+                descriptor += string(rune(int('a') + i))
+            }
+        }
+        descriptorToAnagramGroup[descriptor] = append(descriptorToAnagramGroup[descriptor], s)
+    }
+    groups := make([][]string, len(descriptorToAnagramGroup))
+    i := 0
+    for _, anagramGroup := range descriptorToAnagramGroup {
+        groups[i] = anagramGroup
+        i++
+    }
+    return groups
+}
