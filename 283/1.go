727b8d3a09de8ab3789a24220c87d25912163b3b
Mirat Rafikov <miratworking@gmail.com>
2023-11-11 23:31:31 +0300

    283 go

---
+++ 283/1.go
@@ -0,0 +1,13 @@
+func moveZeroes(nums []int)  {
+    i := 0
+    j := 1
+    for i < len(nums) && j < len(nums) {
+        if nums[i] != 0 {
+            i++
+        } else if nums[j] != 0 {
+            nums[i], nums[j] = nums[j], nums[i]
+            i++
+        }
+        j++
+    }
+}
